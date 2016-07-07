23d56669c5bcd776a2f1d3ae5e18610e1e1cc8f2
Landonius <landon.cline@vml.com>
2016-07-07 09:31:49 -0500

    adding login authentication with auth0

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -1,15 +1,22 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component} from 'angular2/core';
 import {BtnComponent} from "../../partials/btn/btn.component";
+import {AuthService} from "../../../services/authentication/authentication.service";
 
 @Component({
     selector: 'home',
     template: require('./home.html'),
-    directives: [BtnComponent]
+    directives: [BtnComponent],
+    providers:[AuthService]
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-    ngOnInit() {
+    constructor(private auth: AuthService) {}
+    login() {
+        this.auth.login();
+    }
+    logout() {
+        this.auth.logout();
     }
 }
 
